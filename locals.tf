65188b76859f00157ffec0be5b65f60b91cc4641
Rohith Jayawardene <gambol99@gmail.com>
2024-12-05 12:17:57 +0000

    chore: bringing in the upstream changes

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
 
 locals {
-  ## The current region we are provisioning resources in 
+  ## The current region we are provisioning resources in
   region = coalesce(var.region, data.aws_region.current.name)
-  ## Enabled regions to deploy the stackset to 
+  ## Enabled regions to deploy the stackset to
   enabled_regions = var.enabled_regions == null ? [local.region] : sort(var.enabled_regions)
 
-  ## Deployments to be created 
+  ## Deployments to be created
   organization_unit_deployments = flatten([
     for region in local.enabled_regions : [
       for unit in var.organizational_units : {
