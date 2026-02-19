8719c2809c30ec62e66a3f40b5651ce6d4d9081e
Rohith Jayawardene <gambol99@gmail.com>
2026-02-19 18:41:39 +0000

    fix: resolved the length issue()

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
   ## Enabled regions to deploy the stackset to
   enabled_regions = var.enabled_regions == null ? [local.region] : sort(var.enabled_regions)
   ## List of exclude accounts for organizational deployments
-  exclude_accounts = length(try(var.exclude_accounts, [])) > 0 ? var.exclude_accounts : []
+  exclude_accounts = try(var.exclude_accounts, [])
 
   ## Deployments to be created
   organization_unit_deployments = flatten([
